f59b2d12662b399742c51138e769583feb8e542a
josmar29 <salinasjosmar@gmail.com>
2022-12-09 18:23:26 -0800

    version2

--- src/main/java/com/example/finalexam/web/CustomerController.java
+++ src/main/java/com/example/finalexam/web/CustomerController.java
@@ -1,13 +1,19 @@
 package com.example.finalexam.web;
 import com.example.finalexam.entities.Customer;
 import com.example.finalexam.repositories.CustomerRepository;
+import jakarta.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.ui.Model;
+import org.springframework.ui.ModelMap;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import java.util.List;
+
+//https://github.com/josmar29/finalExam
 
 @Controller
 @AllArgsConstructor
@@ -30,6 +36,18 @@ public class CustomerController {
 //        model.addAttribute("student", new Customer());
 //        return "redirect:/customers";
 //    }
+
+    @PostMapping(path = "/save")
+    public String save(Model model, Customer customer, BindingResult bindingResult,  HttpSession session) {
+        if (bindingResult.hasErrors()) {
+            return "editCustomers";
+        } else {
+            customerRepository.save(customer);
+
+
+            return "redirect:index";
+        }
+    }
 
 
 }
